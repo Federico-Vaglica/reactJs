ccf7d07d70e6ac2a2717cfe2c0c294f4631d2d06
federicov <vaglicafederico95@gmail.com>
2020-10-03 10:32:50 +0200

    State&Props

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,37 +1,39 @@
 import React, { useState } from 'react';
 
 
-// import Post from './FacebookPost';
+import Post from './FacebookPost';
 
 
 function App(){
-  // incrementNumber= () => {
-  //   this.setState({
-  //     number: this.state.number + 1
-  //   })
+
+  // const [count, setCount] = useState(5);
+  // const incrementNumber = () => {
+  //   setCount(count + 1)
   // }
-
-  // decrement = () => {
-  //   this.setState({
-  //     number: this.state.number - 1
-  //   })
+  // const decrementNumber = () => {
+  //   setCount(count - 1)
   // }
+  //   return (
+  //     <div className='container'>
+  //       <h4>Counter: {count}</h4>
+  //       <button onClick={incrementNumber}>Aumenta</button>
+  //       <button onClick={decrementNumber}>Decrementa</button>
+  //     </div>
+  //   )
 
-  const [count, setCount] = useState(5);
-  const incrementNumber = () => {
-    setCount(count + 1)
-  }
-  const decrementNumber = () => {
-    setCount(count - 1)
-  }
-    return (
-      <div className='container'>
-        <h4>Counter: {count}</h4>
-        <button onClick={incrementNumber}>Aumenta</button>
-        <button onClick={decrementNumber}>Decrementa</button>
-      </div>
-    )
+  const [posts, setPost] = useState([
+    {author: "Federico" , description: "React is powerFull"},
+    {author: "Marta" , description: "React is awesome"},
+    {author: "Lisa" , description: "Hello Everybody"}
+  ])
 
-};
+  return (
+    <div className="container">
+      {posts.map((post,index) =>(
+        <Post key = {index} author ={post.author} description = {post.description}/>
+      ))}
+    </div>
+  )
+}
 
 export default App;
