2e9ab942e25b6d056f45a7392ab6d4c03cd5155a
federicov <vaglicafederico95@gmail.com>
2020-10-03 09:59:51 +0200

    State

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import Post from './FacebookPost';
+// import Post from './FacebookPost';
 
 
-function App(){
-  return (
-    <div className='container'>
-      <Post author='Federico' description='Good morning React'/>
-      <Post author='Marta' description='Hello Everybody'/>
-      <Post author='Lisa' description='React is nice'/>
-    </div>
-  )
+class App extends React.Component{
+  state = {
+    number: 0,
+  }
+
+  incrementNumber= () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      number: this.state.number - 1
+    })
+  }
+  render(){
+    return (
+      <div className='container'>
+        {/* <Post author='Federico' description='Good morning React'/>
+        <Post author='Marta' description='Hello Everybody'/>
+        <Post author='Lisa' description='React is nice'/> */}
+        <h4>Counter: {this.state.number}</h4>
+        <button onClick={this.incrementNumber}>Aumenta</button>
+        <button onClick={this.decrement}>Decrementa</button>
+      </div>
+    )
+  }
+
 };
 
 export default App;
